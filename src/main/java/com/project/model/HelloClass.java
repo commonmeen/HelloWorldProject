aea11af625c105f0e6565bfc7bb66efd5a96e2bd
kurommnk <e.l.f.eunhyuk@hotmail.com>
2018-02-13 00:56:11 +0700

    [Update] Method in HelloClass

--- src/main/java/com/project/model/HelloClass.java
+++ src/main/java/com/project/model/HelloClass.java
@@ -5,6 +5,13 @@
  */
 package com.project.model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class HelloClass {
     private int id;
     private String message;
@@ -12,6 +19,11 @@ public class HelloClass {
     public HelloClass(){
         
     }
+    
+    public HelloClass(int id){
+        this.id = id;
+    }
+    
     public HelloClass(int id, String message) {
         this.id = id;
         this.message = message;
@@ -37,4 +49,31 @@ public class HelloClass {
     public String toString() {
         return "HelloClass{" + "id=" + id + ", message=" + message + '}';
     }
+    
+    public String pullMessage(int id){
+        Connection con = null;
+        HelloClass hello = null;
+        String message = null;
+        try {
+            con = ConnectionBuilder.getMySqlCond();
+            PreparedStatement pstm = con.prepareStatement("SELECT message FROM HelloClass WHERE id=?");
+            pstm.setInt(1, id);
+            ResultSet rs = pstm.executeQuery();
+            if(rs.next()){
+                message = rs.getString("message");
+            }
+            rs.close();
+            pstm.close();
+            con.close();
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(HelloClass.class.getName()).log(Level.SEVERE, null, ex);
+            message = "404 Class not found";
+        } catch (SQLException ex) {
+            Logger.getLogger(HelloClass.class.getName()).log(Level.SEVERE, null, ex);
+            message = "SQL Exception";
+        }
+        
+        return message;
+    }
+    
 }
